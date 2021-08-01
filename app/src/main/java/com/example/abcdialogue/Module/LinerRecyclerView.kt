a559f1406f92cc9557701fb32d08baab706c7db7
zhaojunjie.passaway <zhaojunjie.passaway@bytedance.com>
2021-08-01 14:55:35 +0800

    eeee

---
+++ app/src/main/java/com/example/abcdialogue/Module/LinerRecyclerView.kt
@@ -0,0 +1,11 @@
+package com.example.abcdialogue.Module
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinerRecyclerView : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_liner_recycler_view)
+    }
+}
